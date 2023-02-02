e2a030357137f8084cae626314fb130185032c19
kame-08 <21cm0108@jec.ac.jp>
2023-02-02 13:56:55 +0900

    Initial Commit

---
+++ kesu2/kesu2App.swift
@@ -0,0 +1,17 @@
+//
+//  kesu2App.swift
+//  kesu2
+//
+//  Created by Ryo on 2023/02/02.
+//
+
+import SwiftUI
+
+@main
+struct kesu2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
